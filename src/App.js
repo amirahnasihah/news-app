aea6e405a66ce7fe74fb762512d1eca329b1a10a
amirahnasihah <amirahnasihah97@gmail.com>
2023-01-17 21:32:26 +0800

    env

--- src/App.js
+++ src/App.js
@@ -7,43 +7,47 @@ import axios from "axios";
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [articless, setArticles] = useState([]);
-  const API_KEY = "6cab899d924340be885932646f30a36c";
+  const [todos, setTodos] = useState([]);
 
   // const handleSearch = (e) => {
   //   setSearchTerm(e.target.value);
   // };
 
+  const options = {
+    method: "GET",
+    url: "https://jsonplaceholder.typicode.com/users/1/todos",
+  };
+
   const handleSearch = async (e) => {
     setSearchTerm(e.target.value);
 
     if (searchTerm === "") {
-      setArticles([]);
+      setTodos([]);
       return;
     }
 
     try {
-      const url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${API_KEY}`;
-      const response = await axios.get(url);
-      setArticles(response.data.articles || []);
+      const response = await axios.request(options);
+      setTodos(response.data);
     } catch (error) {
       console.error(error);
-      setArticles([]);
     }
   };
 
-  const filterArticles = articless.filter((item) => {
-    return item.title.toLowerCase().includes(searchTerm?.toLowerCase() || "");
+  const filterEndpoints = todos.filter((item) => {
+    return item.title
+      .toLowerCase()
+      .includes(searchTerm?.toLowerCase().indexOf(searchTerm) || "");
   });
 
   return (
     <div className="App">
       <h2>Start searching to see some magic happen!🎆</h2>
       <Search searchTerm={searchTerm} handleSearch={handleSearch} />
-      {articless.length === 0 ? (
-        <p>No articles found for the given search term.</p>
+      {todos.length === 0 ? (
+        <p>No movies found for the given search term.</p>
       ) : (
-        <List articles={filterArticles} />
+        <List endpoints={filterEndpoints} />
       )}
     </div>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,24 @@
-function List({ articles }) {
+export default function List({ endpoints }) {
+  const { id, title, completed } = endpoints;
   return (
     <div>
-      {articles.map((article) => (
-        <div key={article.url}>
-          <h3>{article.title}</h3>
-          <a href={article.url} target="_blank" rel="noopener noreferrer">
-            {article.description}
-          </a>
+      {endpoints.map((e) => (
+        <div key={id}>
+          <h3>{title}</h3>
+          {/* <a
+            href="https://jsonplaceholder.typicode.com/users/1/todos"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a> */}
+          <p>
+            {title}
+            {completed}
+          </p>
         </div>
       ))}
     </div>
   );
 }
-
-export default List;
 
 // list === displayresults
