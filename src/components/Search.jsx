9eb3cde801acb523be88f3edee818dfa74dcf853
amirahnasihah <amirahnasihah97@gmail.com>
2023-01-26 21:09:08 +0800

    rm search btn

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,12 +14,12 @@ export default function Search({ searchButton, handleInput, searchInput }) {
             value={searchInput}
             onChange={handleInput}
           />
-          <button
+          {/* <button
             className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={handleInput}
           >
             Search
-          </button>
+          </button> */}
         </div>
       </form>
     </div>
